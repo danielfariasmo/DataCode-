1571a35e1fde3abfd1ee67a4699c96a5f469b3f8
Ignacio <ignacho.mog03@gmail.com>
2024-04-02 13:45:25 +0200

    cambio java

--- Programacion/src/prueba/Pruebas.java
+++ Programacion/src/prueba/Pruebas.java
@@ -5,6 +5,7 @@ public class Pruebas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		System.out.println("Hola NAcho ");
 	}
 
 }
