8e4c9be3034170eb4affde26b5950eb7ef111a06
Ignacio <ignacho.mog03@gmail.com>
2024-04-05 18:42:52 +0200

    Insercion datos en tablas

--- Bases_Datos/DataCode++.BBDD.sql
+++ Bases_Datos/DataCode++.BBDD.sql
@@ -61,3 +61,42 @@ CREATE TABLE Juega (
     FOREIGN KEY (id_personaje) REFERENCES Personaje (id_personaje),
     FOREIGN KEY (id_partida) REFERENCES Partida (id_partida)
     );
+    
+-- INSERCIÓN DE DATOS
+-- Miembros
+INSERT INTO Miembro (id_miembro, nombre_apellidos, numero_expediente, nombre_estudio, nombre_usuario, clave_usuario)
+VALUES (01, 'Nacho Moreno', 456789, 'DAW', 'achonacho', 1234);
+INSERT INTO Miembro (id_miembro, nombre_apellidos, numero_expediente, nombre_estudio, nombre_usuario, clave_usuario)
+VALUES (02, 'Daniel Farias', 876543, 'DAW', 'danielfarias', 0000);
+INSERT INTO Miembro (id_miembro, nombre_apellidos, numero_expediente, nombre_estudio, nombre_usuario, clave_usuario)
+VALUES (03, 'Daniel Gonzalez', 674510, 'DAW', 'garrote', 2323);
+INSERT INTO Miembro (id_miembro, nombre_apellidos, numero_expediente, nombre_estudio, nombre_usuario, clave_usuario)
+VALUES (04, 'Sara Villanueva', 346701, 'Educacion', 'sara', 3470);
+INSERT INTO Miembro (id_miembro, nombre_apellidos, numero_expediente, nombre_estudio, nombre_usuario, clave_usuario)
+VALUES (05, 'Irene Rincon', 561201, 'Educacion', 'irene', 6565);
+INSERT INTO Miembro (id_miembro, nombre_apellidos, numero_expediente, nombre_estudio, nombre_usuario, clave_usuario)
+VALUES (06, 'Raul Rodriguez', 122101, 'Ingeniera', 'raul', 0219);
+
+-- Personajes
+INSERT INTO Personaje (id_personaje, nombre, raza, nivel_experiencia, clase, id_miembro)
+VALUES (21, 'Aragorn', 'Humano', 3, 'Guerrero', 01) ;
+INSERT INTO Personaje (id_personaje, nombre, raza, nivel_experiencia, clase, id_miembro)
+VALUES (11, 'Drako el Sabio', 'Draconido', 4, 'Mago', 01);
+INSERT INTO Personaje (id_personaje, nombre, raza, nivel_experiencia, clase, id_miembro)
+VALUES (20, 'Thranduil', 'Elfo', 2, 'Guerrero', 02);
+INSERT INTO Personaje (id_personaje, nombre, raza, nivel_experiencia, clase, id_miembro)
+VALUES (12, 'Han Solo', 'Humano', 4, 'Contrabandista', 02);
+INSERT INTO Personaje (id_personaje, nombre, raza, nivel_experiencia, clase, id_miembro)
+VALUES (10, 'Frodo Bolson', 'Hobbit', 2, 'Heroe', 03);
+INSERT INTO Personaje (id_personaje, nombre, raza, nivel_experiencia, clase, id_miembro)
+VALUES (16, 'Bofur Barbillas', 'Enano', 3, 'Picaro', 03);
+INSERT INTO Personaje (id_personaje, nombre, raza, nivel_experiencia, clase, id_miembro)
+VALUES (13, 'Sam', 'Mediano', 01, 'Guardian', 03 );
+INSERT INTO Personaje (id_personaje, nombre, raza, nivel_experiencia, clase, id_miembro)
+VALUES (18, 'Grimnir', 'Semiorco', 02, 'Paladin', 05);
+
+-- Game Master
+INSERT INTO GameMaster (id_gameMaster, alias, id_miembro)
+VALUES (66, 'Capitan Raul' , 04);
+INSERT INTO GameMaster (id_gameMaster, alias, id_miembro)
+VALUES (99, 'Sara Durmiente', 06);
