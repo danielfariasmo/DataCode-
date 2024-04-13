38ac6bc39bc7383de9bf1725756cee7dad586016
Ignacio <ignacho.mog03@gmail.com>
2024-04-13 12:13:11 +0200

    Modificacion colores y finalizo menu ppal

--- Programacion/src/vistas/Consultar_Partida.java
+++ Programacion/src/vistas/Consultar_Partida.java
@@ -3,10 +3,6 @@
  */
 package vistas;
 
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.*;
 
 import java.awt.Font;
@@ -36,15 +32,20 @@ public class Consultar_Partida extends JFrame {
 		 */
 		JMenuBar barraMenu = new JMenuBar();
 		barraMenu.setFont(new Font("Verdana", Font.PLAIN, 30));
+		barraMenu.setBackground(new Color(135, 206, 235));
 
 		// Opcion Personaje.
 		JMenu personajes = new JMenu("Personajes");
+		personajes.setForeground(new Color(37, 34, 81));
 		personajes.setFont(new Font("Verdana", Font.BOLD, 25));
 		JMenuItem nuevoPersonaje = new JMenuItem("Nuevo Personaje");
+		nuevoPersonaje.setForeground(new Color(37, 34, 81));
 		nuevoPersonaje.setFont(new Font("Verdana", Font.PLAIN, 25));
 		JMenuItem mostrarPersonajes = new JMenuItem("Mostrar Personajes");
+		mostrarPersonajes.setForeground(new Color(37, 34, 81));
 		mostrarPersonajes.setFont(new Font("Verdana", Font.PLAIN, 25));
 		JMenuItem modificarPersonaje = new JMenuItem("Modificar Personaje");
+		modificarPersonaje.setForeground(new Color(37, 34, 81));
 		modificarPersonaje.setFont(new Font("Verdana", Font.PLAIN, 25));
 		personajes.add(nuevoPersonaje);
 		personajes.add(new JSeparator());
@@ -54,12 +55,16 @@ public class Consultar_Partida extends JFrame {
 
 		// Opcion Partidas.
 		JMenu partidas = new JMenu("Partidas");
+		partidas.setForeground(new Color(37, 34, 81));
 		partidas.setFont(new Font("Verdana", Font.BOLD, 25));
 		JMenuItem crearPartida = new JMenuItem("Crear Partida");
+		crearPartida.setForeground(new Color(37, 34, 81));
 		crearPartida.setFont(new Font("Verdana", Font.PLAIN, 25));
 		JMenuItem consultarPartida = new JMenuItem("Consultar Partida");
+		consultarPartida.setForeground(new Color(37, 34, 81));
 		consultarPartida.setFont(new Font("Verdana", Font.PLAIN, 25));
 		JMenuItem editarPartida = new JMenuItem("Editar Partida");
+		editarPartida.setForeground(new Color(37, 34, 81));
 		editarPartida.setFont(new Font("Verdana", Font.PLAIN, 25));
 		partidas.add(crearPartida);
 		partidas.add(new JSeparator());
@@ -69,8 +74,10 @@ public class Consultar_Partida extends JFrame {
 
 		// Opcion Miembros.
 		JMenu miembros = new JMenu("Miembros");
+		miembros.setForeground(new Color(37, 34, 81));
 		miembros.setFont(new Font("Verdana", Font.BOLD, 25));
 		JMenuItem mostrarMiembros = new JMenuItem("Mostrar Miembros");
+		mostrarMiembros.setForeground(new Color(37, 34, 81));
 		mostrarMiembros.setFont(new Font("Verdana", Font.PLAIN, 25));
 		miembros.add(mostrarMiembros);
 
--- Programacion/src/vistas/MenuPrincipal.java
+++ Programacion/src/vistas/MenuPrincipal.java
@@ -3,15 +3,13 @@
  */
 package vistas;
 
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.*;
 
 import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
 
 /**
  * 
@@ -30,21 +28,39 @@ public class MenuPrincipal extends JFrame {
 		setIconImage(Toolkit.getDefaultToolkit().getImage("img/DC.Icon2.png"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setBackground(new Color(37, 34, 81));
+		getContentPane().setLayout(new FlowLayout(FlowLayout.LEFT, 30, 50));
+
+		// Logo
+		JLabel labelLogo = new JLabel("");
+		labelLogo.setIcon(new ImageIcon("img\\DataCode.Logo.png"));
+		getContentPane().add(labelLogo, BorderLayout.WEST);
+
+		// Introduccion
+		JLabel labelTexto = new JLabel(
+				"<html><div style='text-align: center; width: 350px;'>Adéntrate en el mundo del Club de Rol DataCode++ de la UEM, donde la imaginación cobra vida a través de épicas aventuras. Nuestra aplicación de gestión de personajes te permite crear y dirigir héroes en sus peligrosos viajes, mientras los Game Masters dan forma a mundos llenos de misterio y emoción. Con un simple clic, podrás explorar tierras desconocidas, forjar alianzas y enfrentarte a desafíos que pondrán a prueba tu valentía. ¿Estás listo para sumergirte en un universo donde solo la imaginación es el límite? Entonces únete a nosotros y deja que la aventura comience.</div></html>");
+		labelTexto.setForeground(new Color(255, 255, 255));
+		labelTexto.setFont(new Font("Verdana", Font.PLAIN, 20));
+		getContentPane().add(labelTexto, BorderLayout.EAST);
 
 		/*
 		 * Menu.
 		 */
 		JMenuBar barraMenu = new JMenuBar();
+		barraMenu.setBackground(new Color(135, 206, 235));
 		barraMenu.setFont(new Font("Verdana", Font.PLAIN, 30));
 
 		// Opcion Personaje.
 		JMenu personajes = new JMenu("Personajes");
+		personajes.setForeground(new Color(37, 34, 81));
 		personajes.setFont(new Font("Verdana", Font.BOLD, 25));
 		JMenuItem nuevoPersonaje = new JMenuItem("Nuevo Personaje");
+		nuevoPersonaje.setForeground(new Color(37, 34, 81));
 		nuevoPersonaje.setFont(new Font("Verdana", Font.PLAIN, 25));
 		JMenuItem mostrarPersonajes = new JMenuItem("Mostrar Personajes");
+		mostrarPersonajes.setForeground(new Color(37, 34, 81));
 		mostrarPersonajes.setFont(new Font("Verdana", Font.PLAIN, 25));
 		JMenuItem modificarPersonaje = new JMenuItem("Modificar Personaje");
+		modificarPersonaje.setForeground(new Color(37, 34, 81));
 		modificarPersonaje.setFont(new Font("Verdana", Font.PLAIN, 25));
 		personajes.add(nuevoPersonaje);
 		personajes.add(new JSeparator());
@@ -54,12 +70,16 @@ public class MenuPrincipal extends JFrame {
 
 		// Opcion Partidas.
 		JMenu partidas = new JMenu("Partidas");
+		partidas.setForeground(new Color(37, 34, 81));
 		partidas.setFont(new Font("Verdana", Font.BOLD, 25));
 		JMenuItem crearPartida = new JMenuItem("Crear Partida");
+		crearPartida.setForeground(new Color(37, 34, 81));
 		crearPartida.setFont(new Font("Verdana", Font.PLAIN, 25));
 		JMenuItem consultarPartida = new JMenuItem("Consultar Partida");
+		consultarPartida.setForeground(new Color(37, 34, 81));
 		consultarPartida.setFont(new Font("Verdana", Font.PLAIN, 25));
 		JMenuItem editarPartida = new JMenuItem("Editar Partida");
+		editarPartida.setForeground(new Color(37, 34, 81));
 		editarPartida.setFont(new Font("Verdana", Font.PLAIN, 25));
 		partidas.add(crearPartida);
 		partidas.add(new JSeparator());
@@ -69,13 +89,14 @@ public class MenuPrincipal extends JFrame {
 
 		// Opcion Miembros.
 		JMenu miembros = new JMenu("Miembros");
+		miembros.setForeground(new Color(37, 34, 81));
 		miembros.setFont(new Font("Verdana", Font.BOLD, 25));
 		JMenuItem mostrarMiembros = new JMenuItem("Mostrar Miembros");
+		mostrarMiembros.setForeground(new Color(37, 34, 81));
 		mostrarMiembros.setFont(new Font("Verdana", Font.PLAIN, 25));
 		miembros.add(mostrarMiembros);
 
 		// Añadimos a la barra las opciones.
-
 		barraMenu.add(new JSeparator());
 		barraMenu.add(personajes);
 		barraMenu.add(new JSeparator());
@@ -93,13 +114,22 @@ public class MenuPrincipal extends JFrame {
 
 		// Hacemos visible la ventana para que se ejecute.
 		setVisible(true);
-		
-		
+
 		// DEBEMOS PONER IMAGEN
 	}
 
 	public void hacerVisible() {
 		setVisible(true);
+	}
+
+	// Main de prueba.
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new MenuPrincipal();
+			}
+		});
 	}
 
 }
