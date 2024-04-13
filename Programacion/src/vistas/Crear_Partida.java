2015955a13d3567f77988344435b3300393b9ff8
Ignacio <ignacho.mog03@gmail.com>
2024-04-13 20:29:35 +0200

    Editar partida

--- Programacion/src/vistas/Crear_Partida.java
+++ Programacion/src/vistas/Crear_Partida.java
@@ -92,97 +92,97 @@ public class Crear_Partida extends JFrame {
 
         // Etiquetas para introducir la información
         JLabel labelIdentificador = new JLabel("Identificador único:");
-        labelIdentificador.setFont(new Font("Verdana", Font.PLAIN, 20));
+        labelIdentificador.setFont(new Font("Verdana", Font.BOLD, 20));
         labelIdentificador.setForeground(Color.WHITE);
         labelIdentificador.setBounds(50, 35, 400, 30);
         getContentPane().add(labelIdentificador);
 
         JLabel labelNombrePartida = new JLabel("Nombre de la partida:");
-        labelNombrePartida.setFont(new Font("Verdana", Font.PLAIN, 20));
+        labelNombrePartida.setFont(new Font("Verdana", Font.BOLD, 20));
         labelNombrePartida.setForeground(Color.WHITE);
         labelNombrePartida.setBounds(50, 91, 400, 30);
         getContentPane().add(labelNombrePartida);
 
         JLabel labelIdGameMaster = new JLabel("ID Game Master:");
-        labelIdGameMaster.setFont(new Font("Verdana", Font.PLAIN, 20));
+        labelIdGameMaster.setFont(new Font("Verdana", Font.BOLD, 20));
         labelIdGameMaster.setForeground(Color.WHITE);
         labelIdGameMaster.setBounds(50, 143, 400, 30);
         getContentPane().add(labelIdGameMaster);
 
         JLabel labelAmbientacion = new JLabel("Ambientación:");
-        labelAmbientacion.setFont(new Font("Verdana", Font.PLAIN, 20));
+        labelAmbientacion.setFont(new Font("Verdana", Font.BOLD, 20));
         labelAmbientacion.setForeground(Color.WHITE);
         labelAmbientacion.setBounds(50, 199, 400, 30);
         getContentPane().add(labelAmbientacion);
 
         JLabel labelPersonajes = new JLabel("Personajes que participan:");
-        labelPersonajes.setFont(new Font("Verdana", Font.PLAIN, 20));
+        labelPersonajes.setFont(new Font("Verdana", Font.BOLD, 20));
         labelPersonajes.setForeground(Color.WHITE);
         labelPersonajes.setBounds(50, 255, 400, 30);
         getContentPane().add(labelPersonajes);
 
         JLabel labelDiaHora = new JLabel("Día y hora de la semana:");
-        labelDiaHora.setFont(new Font("Verdana", Font.PLAIN, 20));
+        labelDiaHora.setFont(new Font("Verdana", Font.BOLD, 20));
         labelDiaHora.setForeground(Color.WHITE);
         labelDiaHora.setBounds(50, 304, 400, 30);
         getContentPane().add(labelDiaHora);
 
         JLabel labelDuracionSesion = new JLabel("Duración de la sesión (horas):");
-        labelDuracionSesion.setFont(new Font("Verdana", Font.PLAIN, 20));
+        labelDuracionSesion.setFont(new Font("Verdana", Font.BOLD, 20));
         labelDuracionSesion.setForeground(Color.WHITE);
         labelDuracionSesion.setBounds(50, 350, 400, 30);
         getContentPane().add(labelDuracionSesion);
 
         JLabel labelNumeroSesion = new JLabel("Número de la sesión:");
-        labelNumeroSesion.setFont(new Font("Verdana", Font.PLAIN, 20));
+        labelNumeroSesion.setFont(new Font("Verdana", Font.BOLD, 20));
         labelNumeroSesion.setForeground(Color.WHITE);
         labelNumeroSesion.setBounds(50, 402, 400, 30);
         getContentPane().add(labelNumeroSesion);
 
         JLabel labelEstado = new JLabel("¿Está en curso o terminada? (Si/No):");
-        labelEstado.setFont(new Font("Verdana", Font.PLAIN, 20));
+        labelEstado.setFont(new Font("Verdana", Font.BOLD, 20));
         labelEstado.setForeground(Color.WHITE);
         labelEstado.setBounds(50, 454, 400, 30);
         getContentPane().add(labelEstado);
 
         // Campos de texto para introducir la información
         JTextField textoIdentificador = new JTextField();
-        textoIdentificador.setFont(new Font("Verdana", Font.PLAIN, 20));
+        textoIdentificador.setFont(new Font("Verdana", Font.BOLD, 20));
         textoIdentificador.setBounds(500, 30, 400, 41);
         getContentPane().add(textoIdentificador);
 
         JTextField textoNombrePartida = new JTextField();
-        textoNombrePartida.setFont(new Font("Verdana", Font.PLAIN, 20));
+        textoNombrePartida.setFont(new Font("Verdana", Font.BOLD, 20));
         textoNombrePartida.setBounds(500, 82, 400, 41);
         getContentPane().add(textoNombrePartida);
 
         JTextField textoIdGameMaster = new JTextField();
-        textoIdGameMaster.setFont(new Font("Verdana", Font.PLAIN, 20));
+        textoIdGameMaster.setFont(new Font("Verdana", Font.BOLD, 20));
         textoIdGameMaster.setBounds(500, 134, 400, 41);
         getContentPane().add(textoIdGameMaster);
 
         JTextField textoAmbientacion = new JTextField();
-        textoAmbientacion.setFont(new Font("Verdana", Font.PLAIN, 20));
+        textoAmbientacion.setFont(new Font("Verdana", Font.BOLD, 20));
         textoAmbientacion.setBounds(500, 188, 400, 41);
         getContentPane().add(textoAmbientacion);
 
         JTextField textoPersonajes = new JTextField();
-        textoPersonajes.setFont(new Font("Verdana", Font.PLAIN, 20));
+        textoPersonajes.setFont(new Font("Verdana", Font.BOLD, 20));
         textoPersonajes.setBounds(500, 240, 400, 41);
         getContentPane().add(textoPersonajes);
 
         JTextField textoDiaHora = new JTextField();
-        textoDiaHora.setFont(new Font("Verdana", Font.PLAIN, 20));
+        textoDiaHora.setFont(new Font("Verdana", Font.BOLD, 20));
         textoDiaHora.setBounds(500, 292, 400, 41);
         getContentPane().add(textoDiaHora);
 
         JTextField textoDuracionSesion = new JTextField();
-        textoDuracionSesion.setFont(new Font("Verdana", Font.PLAIN, 20));
+        textoDuracionSesion.setFont(new Font("Verdana", Font.BOLD, 20));
         textoDuracionSesion.setBounds(500, 345, 400, 41);
         getContentPane().add(textoDuracionSesion);
 
         JTextField textoNumeroSesion = new JTextField();
-        textoNumeroSesion.setFont(new Font("Verdana", Font.PLAIN, 20));
+        textoNumeroSesion.setFont(new Font("Verdana", Font.BOLD, 20));
         textoNumeroSesion.setBounds(500, 397, 400, 41);
         getContentPane().add(textoNumeroSesion);
 
--- Programacion/src/vistas/Editar_Partida.java
+++ Programacion/src/vistas/Editar_Partida.java
@@ -17,6 +17,14 @@ public class Editar_Partida extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
 
 	public Editar_Partida() {
 
@@ -26,6 +34,7 @@ public class Editar_Partida extends JFrame {
 		setIconImage(Toolkit.getDefaultToolkit().getImage("img/DC.Icon2.png"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setBackground(new Color(37, 34, 81));
+		getContentPane().setLayout(null);
 
 		/*
 		 * Menu.
@@ -91,6 +100,128 @@ public class Editar_Partida extends JFrame {
 		barraMenu.add(miembros);
 		barraMenu.add(new JSeparator());
 
+		// Etiquetas para introducir la información
+		JLabel labelIdentificador = new JLabel("Identificador único:");
+		labelIdentificador.setFont(new Font("Verdana", Font.BOLD, 19));
+		labelIdentificador.setForeground(Color.WHITE);
+		labelIdentificador.setBounds(50, 35, 258, 40);
+		getContentPane().add(labelIdentificador);
+
+		JLabel labelNombrePartida = new JLabel("Nombre de la partida:");
+		labelNombrePartida.setFont(new Font("Verdana", Font.BOLD, 19));
+		labelNombrePartida.setForeground(Color.WHITE);
+		labelNombrePartida.setBounds(50, 91, 258, 40);
+		getContentPane().add(labelNombrePartida);
+
+		JLabel labelIdGameMaster = new JLabel("ID Game Master:");
+		labelIdGameMaster.setFont(new Font("Verdana", Font.BOLD, 19));
+		labelIdGameMaster.setForeground(Color.WHITE);
+		labelIdGameMaster.setBounds(50, 143, 258, 40);
+		getContentPane().add(labelIdGameMaster);
+
+		JLabel labelAmbientacion = new JLabel("Ambientación:");
+		labelAmbientacion.setFont(new Font("Verdana", Font.BOLD, 19));
+		labelAmbientacion.setForeground(Color.WHITE);
+		labelAmbientacion.setBounds(50, 199, 400, 30);
+		getContentPane().add(labelAmbientacion);
+
+		JLabel labelPersonajes = new JLabel("Personajes que participan:");
+		labelPersonajes.setFont(new Font("Verdana", Font.BOLD, 19));
+		labelPersonajes.setForeground(Color.WHITE);
+		labelPersonajes.setBounds(50, 255, 400, 30);
+		getContentPane().add(labelPersonajes);
+
+		JLabel labelDiaHora = new JLabel("Día y hora de la semana:");
+		labelDiaHora.setFont(new Font("Verdana", Font.BOLD, 19));
+		labelDiaHora.setForeground(Color.WHITE);
+		labelDiaHora.setBounds(50, 304, 400, 30);
+		getContentPane().add(labelDiaHora);
+
+		JLabel labelDuracionSesion = new JLabel("Duración de la sesión (horas):");
+		labelDuracionSesion.setFont(new Font("Verdana", Font.BOLD, 19));
+		labelDuracionSesion.setForeground(Color.WHITE);
+		labelDuracionSesion.setBounds(50, 350, 400, 30);
+		getContentPane().add(labelDuracionSesion);
+
+		JLabel labelNumeroSesion = new JLabel("Número de la sesión:");
+		labelNumeroSesion.setFont(new Font("Verdana", Font.BOLD, 19));
+		labelNumeroSesion.setForeground(Color.WHITE);
+		labelNumeroSesion.setBounds(50, 402, 400, 30);
+		getContentPane().add(labelNumeroSesion);
+
+		JLabel labelEstado = new JLabel("¿Está en curso o terminada? (Si/No):");
+		labelEstado.setFont(new Font("Verdana", Font.BOLD, 19));
+		labelEstado.setForeground(Color.WHITE);
+		labelEstado.setBounds(50, 454, 407, 30);
+		getContentPane().add(labelEstado);
+
+		// Botón para registrarse
+		JButton botonCrearPartida = new JButton("Editar Partida");
+		botonCrearPartida.setFont(new Font("Verdana", Font.BOLD, 20));
+		botonCrearPartida.setForeground(new Color(37, 34, 81));
+		botonCrearPartida.setBackground(new Color(135, 206, 235));
+		botonCrearPartida.setBounds(500, 514, 400, 50);
+		getContentPane().add(botonCrearPartida);
+
+		// Botones de seleccion
+		JRadioButton radioSi = new JRadioButton("Si");
+		JRadioButton radioNo = new JRadioButton("No");
+		ButtonGroup group = new ButtonGroup();
+		group.add(radioSi);
+		group.add(radioNo);
+
+		radioSi.setFont(new Font("Verdana", Font.PLAIN, 20));
+		radioSi.setForeground(Color.WHITE);
+		radioSi.setBackground(new Color(37, 34, 81));
+		radioSi.setBounds(539, 454, 70, 30);
+		getContentPane().add(radioSi);
+
+		radioNo.setFont(new Font("Verdana", Font.PLAIN, 20));
+		radioNo.setForeground(Color.WHITE);
+		radioNo.setBackground(new Color(37, 34, 81));
+		radioNo.setBounds(707, 454, 70, 30);
+		getContentPane().add(radioNo);
+		
+		textField = new JTextField();
+		textField.setBounds(531, 39, 258, 40);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(531, 91, 258, 40);
+		getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(531, 143, 258, 40);
+		getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(531, 198, 258, 40);
+		getContentPane().add(textField_3);
+		textField_3.setColumns(10);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(531, 254, 258, 40);
+		getContentPane().add(textField_4);
+		textField_4.setColumns(10);
+		
+		textField_5 = new JTextField();
+		textField_5.setBounds(531, 308, 258, 40);
+		getContentPane().add(textField_5);
+		textField_5.setColumns(10);
+		
+		textField_6 = new JTextField();
+		textField_6.setBounds(531, 359, 258, 40);
+		getContentPane().add(textField_6);
+		textField_6.setColumns(10);
+		
+		textField_7 = new JTextField();
+		textField_7.setBounds(531, 411, 258, 40);
+		getContentPane().add(textField_7);
+		textField_7.setColumns(10);
+
 		// Configuramos la barra de menú al JFrame
 		setJMenuBar(barraMenu);
 
@@ -100,7 +231,7 @@ public class Editar_Partida extends JFrame {
 
 		// Hacemos visible la ventana para que se ejecute.
 		setVisible(true);
-		
+
 		System.out.println("daniel f");
 	}
 
@@ -113,5 +244,4 @@ public class Editar_Partida extends JFrame {
 			}
 		});
 	}
-
 }
