5b678b7daf3a7c03337c10066db4da5807b50747
danielfariasmo <fariasd99@gmail.com>
2024-04-20 22:25:35 +0200

    Modifico menu GameMaster, creo carpeta JAR

--- Programacion/src/vistas/GameMaster.java
+++ Programacion/src/vistas/GameMaster.java
@@ -1,28 +1,25 @@
 package vistas;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Toolkit;
-import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JSeparator;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 /**
- * 
+ * @author Daniel F.
+ * @author Ignacio M.
+ * @author Daniel G.
  */
 public class GameMaster extends JFrame {
 	
@@ -35,21 +32,21 @@ public class GameMaster extends JFrame {
 		setTitle("DataCode++ (Editar Partida)");
 		setIconImage(Toolkit.getDefaultToolkit().getImage("img/DC.Icon2.png"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		getContentPane().setBackground(new Color(37, 34, 81));
+		getContentPane().setBackground(new Color(255, 255, 255));
 		getContentPane().setLayout(null); // Layout null para posicionar elementos manualmente
 
 		// Texto en el centro de la ventana
-		JLabel textoCentral = new JLabel("¡No estás registrado como GameMaster!");
-		textoCentral.setFont(new Font("Verdana", Font.BOLD, 36));
+		JLabel textoCentral = new JLabel("Ups.. !No eres GameMaster!");
+		textoCentral.setFont(new Font("Verdana", Font.BOLD, 30));
 		textoCentral.setForeground(Color.WHITE);
-		textoCentral.setBounds(115, 44, 921, 50);
+		textoCentral.setBounds(539, 77, 487, 50);
 		getContentPane().add(textoCentral);
 
 		// Panel para el texto "Escribe tu alias"
 		JPanel panelAlias = new JPanel();
 		panelAlias.setBackground(new Color(37, 34, 81));
 		panelAlias.setLayout(new BorderLayout());
-		panelAlias.setBounds(628, 336, 350, 50);
+		panelAlias.setBounds(597, 337, 350, 50);
 		getContentPane().add(panelAlias);
 
 		// Botón para registrarse
@@ -57,14 +54,14 @@ public class GameMaster extends JFrame {
 		botonRegistrarse.setFont(new Font("Verdana", Font.BOLD, 20));
 		botonRegistrarse.setForeground(new Color(37, 34, 81));
 		botonRegistrarse.setBackground(new Color(135, 206, 235));
-		botonRegistrarse.setBounds(697, 424, 216, 50);
+		botonRegistrarse.setBounds(670, 426, 216, 50);
 		getContentPane().add(botonRegistrarse);
 		
 		//Label alias
 		JLabel labelAlias = new JLabel("Escribe tu \"Alias\" :");
-		labelAlias.setBounds(649, 202, 303, 50);
+		labelAlias.setBounds(624, 230, 303, 50);
 		getContentPane().add(labelAlias);
-		labelAlias.setFont(new Font("Verdana", Font.BOLD, 25));
+		labelAlias.setFont(new Font("Verdana", Font.PLAIN, 30));
 		labelAlias.setForeground(Color.WHITE);
 		
 		// Label mostrar mensaje al usuario
@@ -76,15 +73,26 @@ public class GameMaster extends JFrame {
 		
 		//Texto para escribir el alias
 		JTextField textoAlias = new JTextField();
-		textoAlias.setBounds(628, 286, 350, 50);
+		textoAlias.setBounds(597, 290, 350, 50);
 		getContentPane().add(textoAlias);
 		textoAlias.setFont(new Font("Verdana", Font.PLAIN, 20));
 		textoAlias.setPreferredSize(new Dimension(200, 40));
 		
 		JLabel etiquetaLogo = new JLabel(" ");
-		etiquetaLogo.setBounds(60, 126, 496, 464);
+		etiquetaLogo.setBounds(10, 0, 519, 612);
 		etiquetaLogo.setIcon(new ImageIcon("img/DataCode.Logo.png"));
 		getContentPane().add(etiquetaLogo);
+		
+		JLabel labelRegistrate = new JLabel("REGISTRATE");
+		labelRegistrate.setForeground(new Color(255, 255, 255));
+		labelRegistrate.setFont(new Font("Verdana", Font.BOLD, 30));
+		labelRegistrate.setBounds(670, 137, 223, 43);
+		getContentPane().add(labelRegistrate);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(37, 34, 81));
+		panel.setBounds(522, 0, 514, 613);
+		getContentPane().add(panel);
 
 		// Crear un cuadro de diálogo personalizado
 		botonRegistrarse.addActionListener(new ActionListener() {
--- Programacion/src/vistas/MenuPrincipalGameMaster.java
+++ Programacion/src/vistas/MenuPrincipalGameMaster.java
@@ -3,7 +3,23 @@
  */
 package vistas;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+
+import control.ControlMenuPrincipalUsuario;
 
 /**
  * 
@@ -15,9 +31,163 @@ public class MenuPrincipalGameMaster extends JFrame {
 	 * @author Ignacio M.
 	 * @author Daniel G.
 	 */
-private static final long serialVersionUID = 1L;
-	
-	
+	private static final long serialVersionUID = 1L;
+
+	JPanel panelPrincipal;
+	JScrollPane scrollPrincipal;
+	JMenuItem nuevoPersonaje;
+	JMenuItem mostrarPersonajes;
+	JMenuItem modificarPersonaje;
+	JMenuItem crearPartida;
+	JMenuItem consultarPartida;
+	JMenuItem editarPartida;
+	JMenuItem mostrarMiembros;
+
+	public MenuPrincipalGameMaster() {
+
+		panelPrincipal = new JPanel();
+		scrollPrincipal = new JScrollPane();
+		getContentPane().add(scrollPrincipal);
+
+		// Barra Superior.
+		setFont(new Font("Verdana", Font.BOLD, 20));
+		setTitle("DataCode++");
+		setIconImage(Toolkit.getDefaultToolkit().getImage("img/DC.Icon2.png"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panelPrincipal.setBackground(new Color(37, 34, 81));
+		panelPrincipal.setLayout(new FlowLayout(FlowLayout.LEFT, 30, 50));
+
+		// Logo
+		JLabel labelLogo = new JLabel("");
+		labelLogo.setIcon(new ImageIcon("img\\DataCode.Logo.png"));
+		panelPrincipal.add(labelLogo, BorderLayout.WEST);
+
+		// Introduccion
+		JLabel labelTexto = new JLabel(
+				"<html><div style='text-align: center; width: 350px;'>Adéntrate en el mundo del Club de Rol DataCode++ de la UEM, donde la imaginación cobra vida a través de épicas aventuras. Nuestra aplicación de gestión de personajes te permite crear y dirigir héroes en sus peligrosos viajes, mientras los Game Masters dan forma a mundos llenos de misterio y emoción. Con un simple clic, podrás explorar tierras desconocidas, forjar alianzas y enfrentarte a desafíos que pondrán a prueba tu valentía. ¿Estás listo para sumergirte en un universo donde solo la imaginación es el límite? Entonces únete a nosotros y deja que la aventura comience.</div></html>");
+		labelTexto.setForeground(new Color(255, 255, 255));
+		labelTexto.setFont(new Font("Verdana", Font.PLAIN, 20));
+		panelPrincipal.add(labelTexto, BorderLayout.EAST);
+
+		/*
+		 * Menu.
+		 */
+		JMenuBar barraMenu = new JMenuBar();
+		barraMenu.setBackground(new Color(135, 206, 235));
+		barraMenu.setFont(new Font("Verdana", Font.PLAIN, 30));
+
+		/*
+		 * Opcion Personaje.
+		 */
+		JMenu personajes = new JMenu("Personajes");
+		personajes.setForeground(new Color(37, 34, 81));
+		personajes.setFont(new Font("Verdana", Font.BOLD, 25));
+
+		// Nuevo Personaje
+		nuevoPersonaje = new JMenuItem("Nuevo Personaje");
+		nuevoPersonaje.setForeground(new Color(37, 34, 81));
+		nuevoPersonaje.setFont(new Font("Verdana", Font.PLAIN, 25));
+
+		// Mostrar Personaje
+		mostrarPersonajes = new JMenuItem("Mostrar Personajes");
+		mostrarPersonajes.setForeground(new Color(37, 34, 81));
+		mostrarPersonajes.setFont(new Font("Verdana", Font.PLAIN, 25));
+
+		// Modificar Personaje
+		modificarPersonaje = new JMenuItem("Modificar Personaje");
+		modificarPersonaje.setForeground(new Color(37, 34, 81));
+		modificarPersonaje.setFont(new Font("Verdana", Font.PLAIN, 25));
+
+		// Añadimos a la barra las opciones.
+		personajes.add(nuevoPersonaje);
+		personajes.add(new JSeparator());
+		personajes.add(mostrarPersonajes);
+		personajes.add(new JSeparator());
+		personajes.add(modificarPersonaje);
+
+		/*
+		 * Opcion Partidas.
+		 */
+		JMenu partidas = new JMenu("Partidas");
+		partidas.setForeground(new Color(37, 34, 81));
+		partidas.setFont(new Font("Verdana", Font.BOLD, 25));
+
+		// Crear Partida
+		crearPartida = new JMenuItem("Crear Partida");
+		crearPartida.setForeground(new Color(37, 34, 81));
+		crearPartida.setFont(new Font("Verdana", Font.PLAIN, 25));
+
+		// Consultar Partida
+		consultarPartida = new JMenuItem("Consultar Partida");
+		consultarPartida.setForeground(new Color(37, 34, 81));
+		consultarPartida.setFont(new Font("Verdana", Font.PLAIN, 25));
+
+		// Editar Partida
+		editarPartida = new JMenuItem("Editar Partida");
+		editarPartida.setForeground(new Color(37, 34, 81));
+		editarPartida.setFont(new Font("Verdana", Font.PLAIN, 25));
+
+		// Añadimos a la barra las opciones.
+		partidas.add(crearPartida);
+		partidas.add(new JSeparator());
+		partidas.add(consultarPartida);
+		partidas.add(new JSeparator());
+		partidas.add(editarPartida);
+
+		/*
+		 * Opcion Miembros.
+		 */
+
+		JMenu miembros = new JMenu("Miembros");
+		miembros.setForeground(new Color(37, 34, 81));
+		miembros.setFont(new Font("Verdana", Font.BOLD, 25));
+
+		// Mostrar miembros.
+		mostrarMiembros = new JMenuItem("Mostrar Miembros");
+		mostrarMiembros.setForeground(new Color(37, 34, 81));
+		mostrarMiembros.setFont(new Font("Verdana", Font.PLAIN, 25));
+
+		// Añadimos opcion a parte del menu Miembros.
+		miembros.add(mostrarMiembros);
+
+		// Añadimos a la barra principal las opciones.
+		barraMenu.add(new JSeparator());
+		barraMenu.add(personajes);
+		barraMenu.add(new JSeparator());
+		barraMenu.add(partidas);
+		barraMenu.add(new JSeparator());
+		barraMenu.add(miembros);
+		barraMenu.add(new JSeparator());
+
+		// Configuramos la barra de menú al JFrame
+		setJMenuBar(barraMenu);
+
+		// Tamaño y posición de la ventana.
+		setSize(1065, 660);
+		setLocationRelativeTo(null);
+
+		cambiarPanel(panelPrincipal);
+		// Hacemos visible la ventana para que se ejecute.
+		setVisible(true);
+
+	}
+
+	public void hacerVisible() {
+		setVisible(true);
+	}
+
+	public void cambiarPanel(JPanel panel) {
+		scrollPrincipal.setViewportView(panel);
+	}
+
+	public void setListener(ControlMenuPrincipalUsuario listener) {
+		nuevoPersonaje.addActionListener(listener);
+		mostrarPersonajes.addActionListener(listener);
+		modificarPersonaje.addActionListener(listener);
+		crearPartida.addActionListener(listener);
+		consultarPartida.addActionListener(listener);
+		editarPartida.addActionListener(listener);
+		mostrarMiembros.addActionListener(listener);
+	}
 
 }
-
