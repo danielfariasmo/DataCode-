1a4ed9f1abc447f5c1cccc2ecc064dfd0ff15cac
Ignacio <ignacho.mog03@gmail.com>
2024-04-26 13:50:23 +0200

    Correccion errores

--- Programacion/src/vistas/ConsultarPartida.java
+++ Programacion/src/vistas/ConsultarPartida.java
@@ -35,16 +35,17 @@ public class ConsultarPartida extends JPanel {
 
 		// Tabla Partida
 		tablePartida = new JTable(new DefaultTableModel(
-				new Object[][] { {"La búsqueda del anillo", "2024-04-01 / 10:30h", "16:00h", 1, "Tierra Media", "No"},
-						{"Galaxia en guerra", "2024-04-02 / 20:00h", "23:50h", 1, "Galaxia", "No"},
-						{"Las sombras de la montaña", "2024-04-03 / 15:00h", "22:00h", 1, "Montañas", "No"},
-						{"La magia perdida", "2024-04-04 / 10:30h", "16:30h", 1, "Bosque encantado", "No"},
-						{"El rescate de la princesa", "2024-04-05 / 08:00h", "12:00h", 1, "Castillo", "No"},
-						{"Intriga en la corte", "2024-04-06 / 14:00h", "20:00h", 1, "Palacio real", "No"},
-						{"La ciudad maldita", "2024-04-07 / 12:30h", "18:00h", 1, "Ciudad abandonada", "No"},
-						{"El laberinto de la muerte", "2024-04-08 / 15:00h", "23:00h", 1, "Laberinto", "No"} },
+				new Object[][] { {"La búsqueda del anillo", "01-04-2024 / 10:30h", "06-04-2024 / 16:00h", 1, "Tierra Media", "No"},
+						{"Galaxia en guerra", "02-04-2024 / 20:00h", "10-04-2024 / 23:50h", 1, "Galaxia", "No"},
+						{"Las sombras de la montaña", "03-02-2024 / 15:00h", "06-04-2024 / 22:00h", 1, "Montañas", "No"},
+						{"La magia perdida", "04-04-2024 / 10:30h", "11-04-2024 / 16:30h", 1, "Bosque encantado", "No"},
+						{"El rescate de la princesa", "05-04-2024 / 08:00h", "12-04-2024 / 12:00h", 1, "Castillo", "No"},
+						{"Intriga en la corte", "06-04-2024 / 14:00h", "15-04-2024 / 20:00h", 1, "Palacio real", "No"},
+						{"La ciudad maldita", "07-04-2024 / 12:30h", "12-04-2024 / 19:00h", 1, "Ciudad abandonada", "No"},
+						{"El laberinto de la muerte", "08-04-2024 / 15:00h", "09-04-2024 / 20:00h", 1, "Laberinto", "No"} },
 				new String[] { "Nombre", "Día y hora de Creación", "Próxima Sesión", "Nº Sesión", "Ambientación", "Finalizada (Si/No)",
 						}));
+		
 		tablePartida.setFont(new Font("Verdana", Font.PLAIN, 15));
 		tablePartida.setForeground(new Color(255, 255, 255));
 		tablePartida.setBackground(new Color(37, 34, 81));
@@ -52,17 +53,17 @@ public class ConsultarPartida extends JPanel {
 		add(tablePartida);
 
 		// Ajustar tamaño preferido de las columnas para que los nombres sean visibles
-		tablePartida.getColumnModel().getColumn(0).setPreferredWidth(190);
-		tablePartida.getColumnModel().getColumn(1).setPreferredWidth(150);
-		tablePartida.getColumnModel().getColumn(2).setPreferredWidth(60);
-		tablePartida.getColumnModel().getColumn(3).setPreferredWidth(35);
+		tablePartida.getColumnModel().getColumn(0).setPreferredWidth(450);
+		tablePartida.getColumnModel().getColumn(1).setPreferredWidth(450);
+		tablePartida.getColumnModel().getColumn(2).setPreferredWidth(450);
+		tablePartida.getColumnModel().getColumn(3).setPreferredWidth(10);
 		tablePartida.getColumnModel().getColumn(4).setPreferredWidth(130);
-		tablePartida.getColumnModel().getColumn(5).setPreferredWidth(75);
+		tablePartida.getColumnModel().getColumn(5).setPreferredWidth(10);
 
 		// Agregar la tabla a un JScrollPane para permitir desplazamiento si es
 		// necesario
 		JScrollPane scrollPane = new JScrollPane(tablePartida);
-		scrollPane.setBounds(40, 150, 952, 153);
+		scrollPane.setBounds(30, 152, 952, 153);
 		add(scrollPane);
 
 		// Contenedor para recuadro blanco
